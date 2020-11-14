2212f14d328e977e4d63899e0fc5d16c0788a405
Sam Gielis <sam.gielis@trustedfamily.net>
2020-11-14 21:44:31 +0100

    Implement basic GameStageView
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/components/GameStageView.tsx
+++ src/components/GameStageView.tsx
@@ -1,15 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import Countdown from "./CountDown";
 import { Stage } from "../model/Stage";
-import { Text } from "@chakra-ui/react"
+import { Box, Button, Input, Text, VStack } from "@chakra-ui/react"
 
 export interface GameStageViewProps {
     stage: Stage
     onStageCompleted: () => void
 }
 
-export const GameStageView = ({ stage }: GameStageViewProps) => {
+export const GameStageView = ({ stage, onStageCompleted }: GameStageViewProps) => {
     return <Countdown date={stage.startDate}>
-        <Text>{stage.question}</Text>
+        <UnlockedStageView stage={stage} onStageCompleted={onStageCompleted} />
     </Countdown>
 }
+
+const UnlockedStageView = ({ stage, onStageCompleted }: GameStageViewProps) => {
+    const [stageComplete, setStageComplete] = useState(false);
+    return (
+        <Box>
+            { !stageComplete
+                ? <StageQuestionView stage={stage} onCorrectAnswer={() => { setStageComplete(true) }} />
+                : <EndOfStageView stage={stage} onStageCompleted={onStageCompleted} />
+            }
+        </Box>
+    )
+};
+
+interface StageQuestionViewProps {
+    stage: Stage;
+    onCorrectAnswer: () => void
+}
+
+const StageQuestionView = ({ stage, onCorrectAnswer }: StageQuestionViewProps) => (
+    <VStack spacing={4}>
+        <Text>{stage.question}</Text>
+        <Input onChange={({ target }) => {
+            if (target.value === stage.answer) {
+                onCorrectAnswer();
+            }
+        }} />
+    </VStack>
+);
+
+const EndOfStageView = ({ stage, onStageCompleted }: GameStageViewProps) => (
+    <VStack spacing={4}>
+        <Text>{`Correct. Dit is jullie clue: ${stage.clue}`}</Text>
+        <Button onClick={onStageCompleted}>Volgende uitdaging</Button>
+    </VStack>
+)
