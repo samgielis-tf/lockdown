cd386654e14f5f7c532428d18e89559e765c0744
Sam Gielis <sam.gielis@trustedfamily.net>
2020-11-14 21:50:31 +0100

    Add dummy stages
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/data/stages.ts
+++ src/data/stages.ts
@@ -1,30 +1,30 @@
 import { Stage } from "../model/Stage";
 
 const Stage1 = {
-    startDate: new Date("Nov 15, 2020 19:30:00"),
+    startDate: new Date("Nov 14, 2020 21:49:00"),
     question: "V2hvIGlzIHlvdXIgbW9t",
-    answer: "",
+    answer: "R3JldGE=",
     clue: "TG9jYXRpZQ=="
 };
 
 const Stage2 = {
-    startDate: new Date("Nov 16, 2020 19:30:00"),
-    question: "",
-    answer: "",
+    startDate: new Date("Nov 13, 2020 19:30:00"),
+    question: "V2hvIGlzIHlvdXIgc2lz",
+    answer: "U3V6YW5uZQ==",
     clue: "aXM="
 };
 
 const Stage3 = {
     startDate: new Date("Nov 17, 2020 19:30:00"),
     question: "V2hvIGlzIHlvdXIgZGFk",
-    answer: "",
+    answer: "Sm9z",
     clue: "V2hpdGU="
 };
 
 const Stage4 = {
     startDate: new Date("Nov 18, 2020 19:30:00"),
-    question: "",
-    answer: "",
+    question: "V2hvIGlzIHlvdXIgZ2lybA==",
+    answer: "U2hhbm5h",
     clue: "SG91c2U="
 };
 
