d491f62f17df4a188fa0fb6bdc16b9727ffabf9f
Sam Gielis <sam.gielis@trustedfamily.net>
2020-11-15 16:01:59 +0100

    Add dummy questions, set proper stage dates
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/data/stages.ts
+++ src/data/stages.ts
@@ -1,30 +1,30 @@
 import { Stage } from "../model/Stage";
 
 const Stage1 = {
-    startDate: new Date("Nov 14, 2020 21:49:00"),
-    question: "V2hvIGlzIHlvdXIgbW9t",
-    answer: "R3JldGE=",
+    startDate: new Date("Nov 15, 2020 19:30:00"),
+    question: "V2llIGlzIGRlIHNsaW1zdGU/",
+    answer: "QmVydA==",
     clue: "TG9jYXRpZQ=="
 };
 
 const Stage2 = {
-    startDate: new Date("Nov 13, 2020 19:30:00"),
-    question: "V2hvIGlzIHlvdXIgc2lz",
-    answer: "U3V6YW5uZQ==",
+    startDate: new Date("Nov 16, 2020 19:30:00"),
+    question: "V2llIGlzIGRlIGtuYXBzdGU/",
+    answer: "U2hhbm5hICYgTWFyaWU=",
     clue: "aXM="
 };
 
 const Stage3 = {
     startDate: new Date("Nov 17, 2020 19:30:00"),
-    question: "V2hvIGlzIHlvdXIgZGFk",
-    answer: "Sm9z",
+    question: "V2F0IGlzIGRlIGxldWtzdGUgc3RhZD8=",
+    answer: "TGV1dmVu",
     clue: "V2hpdGU="
 };
 
 const Stage4 = {
     startDate: new Date("Nov 18, 2020 19:30:00"),
-    question: "V2hvIGlzIHlvdXIgZ2lybA==",
-    answer: "U2hhbm5h",
+    question: "V2F0IGlzIGhldCBiZXN0ZSBiaWVyPw==",
+    answer: "U3RlbGxh",
     clue: "SG91c2U="
 };
 
