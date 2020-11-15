5dd6daf7877006ce3e6d33e1040e9c9662f8042c
Sam Gielis <sam.gielis@trustedfamily.net>
2020-11-15 11:14:35 +0100

    Change bg and font color
    Signed-off-by: Sam Gielis <sam.gielis@trustedfamily.net>

--- src/App.tsx
+++ src/App.tsx
@@ -11,8 +11,8 @@ import { game } from "./data/game"
 
 export const App = () => (
   <ChakraProvider theme={theme}>
-    <Box textAlign="center" fontSize="xl">
-      <Grid minH="100vh" p="auto">
+    <Box textAlign="center" fontSize="xl" background="gray.900" color="green.400">
+      <Grid minH="100vh" p={10}>
         <VStack spacing={8}>
           <GameView game={game} />
         </VStack>
